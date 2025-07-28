08e4d8ec77a2493011cc0d6345bfdece61b6d105
Neurarian <110474238+Neurarian@users.noreply.github.com>
2025-07-28 14:26:03 +0200

    fix: install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ while true; do
   read -p "Do you want create directories needed for end-4 colorgen scripts (y/n) " yn
   case $yn in
     [yY]) echo "Creating directories..."
-          mkdir -p $$HOME/.local/state/ags/{scss,user} $HOME/.cache/ags/user/generated
+          mkdir -p $HOME/.local/state/ags/{scss,user} $HOME/.cache/ags/user/generated
           break
           ;;
     [nN]) echo "Skipping..."

86fc13a728def33e468ab664c4241bb954da999e
Neurarian <110474238+Neurarian@users.noreply.github.com>
2025-09-28 23:11:17 +0200

    remove: redundant code

--- utils/picker/types.ts
+++ utils/picker/types.ts
@@ -42,23 +42,6 @@ export interface ISearchProvider<T> {
   getThumbnail?(imagePath: string): Promise<Gdk.Texture | null>;
 }
 
-export interface CachedThumbnail {
-  texture: Gdk.Texture;
-  timestamp: number;
-  lastAccessed: number;
-}
-
-export interface ThemeProperties {
-  tone: number;
-  chroma: number;
-  mode: "light" | "dark";
-  scheme: "scheme-neutral" | "scheme-vibrant";
-}
-
-export interface CachedThemeEntry extends ThemeProperties {
-  timestamp: number;
-}
-
 // Type aliases
 
 export type ComponentType = "list" | "grid";
